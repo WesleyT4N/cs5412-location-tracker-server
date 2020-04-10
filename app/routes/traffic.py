415fe9d9b1f9ae1adce1ad92504a0e254ad1a0bd
Wes T4N <wesleyt1998@gmail.com>
2020-04-10 18:37:33 -0400

    WIP traffic routes, tests done

--- app/routes/traffic.py
+++ app/routes/traffic.py
@@ -14,42 +14,59 @@ from app.routes.locations import get_location
 
 # TODO: For scaling purposes, we may want to introduce a caching layer for these traffic readings (~15 min TTL)
 
-class DatastoreStatisticsEnum(Enum):
+class DatastoreEndpointEnum(Enum):
     """
     List of possible statistics we can retrieve from the data-store
     """
 
-    TRAFFIC_COUNT = "CURRENT_TRAFFIC_COUNT"
-    PEAK_TRAFFIC = "PEAK_TRAFFIC"
-    TRAFFIC_HISTORY = "TRAFFIC_HISTORY"
+    TRAFFIC_COUNT = "/api/traffic_count"
+    PEAK_TRAFFIC = "/api/peak_traffic"
+    TRAFFIC_HISTORY = "/api/traffic_history"
 
 traffic_bp = Blueprint("traffic", __name__, url_prefix="/api/locations/<location_id>/")
 
 class BaseTrafficSchema(Schema):
-    fetched_at = fields.DateTime(default=dt.datetime.utcnow(), data_key="fetchedAt")
-    location_id = fields.UUID(data_key="locationId")
+    fetched_at = fields.DateTime(default=dt.datetime.utcnow(), data_key="fetchedAt", required=True)
+    location_id = fields.UUID(data_key="locationId", required=True)
 
 class TrafficCountSchema(BaseTrafficSchema):
-    time = fields.DateTime()
-    traffic_count = fields.Int(data_key="trafficCount")
+    time = fields.DateTime(required=True)
+    traffic_count = fields.Int(data_key="trafficCount", required=True)
 
 class TrafficCountInputSchema(Schema):
     time = fields.DateTime(default=dt.datetime.utcnow(), required=False)
     sensor_ids = fields.List(fields.UUID(), required=False)
+    location_id = fields.UUID(required=False)
 
 class PeakTrafficSchema(BaseTrafficSchema):
+    start_time = fields.DateTime(required=True, data_key="startTime")
+    end_time = fields.DateTime(required=True, data_key="endTime")
     peak_traffic = fields.Nested(
         TrafficCountSchema,
         only=("time", "traffic_count"),
-        data_key="peakTrafficTime",
+        data_key="peakTraffic",
+        required=True,
     )
 
+class PeakTrafficInputSchema(Schema):
+    start_time = fields.DateTime(required=True)
+    end_time = fields.DateTime(required=True)
+    sensor_ids = fields.List(fields.UUID(), required=False)
+    location_id = fields.UUID(required=False)
+
 class TrafficHistorySchema(BaseTrafficSchema):
+    start_time = fields.DateTime(required=True, data_key="startTime")
+    end_time = fields.DateTime(required=True, data_key="endTime")
     traffic_history = fields.List(fields.Nested(
         TrafficCountSchema,
         only=("time", "traffic_count"),
-        data_key="trafficHistory",
-    ))
+    ), data_key="trafficHistory")
+
+class TrafficHistoryInputSchema(Schema):
+    start_time = fields.DateTime(required=True)
+    end_time = fields.DateTime(required=True)
+    sensor_ids = fields.List(fields.UUID(), required=False)
+    location_id = fields.UUID(required=False)
 
 def get_sensor_ids(location_id):
     location = get_location(location_id)
@@ -64,30 +81,35 @@ def get_from_data_store(endpoint, args):
     url = current_app.config["DATA_STORE_BASE_URL"] + endpoint
     return requests.get(url, params=args)
 
-@traffic_bp_route("traffic_count", methods["GET"])
+@traffic_bp.route("traffic_count", methods=["GET"])
 def get_traffic_count(location_id):
-    request.args["locationId"] = location_id
     input_schema = TrafficCountInputSchema()
     try:
-        args = input_schema.load(request.args)
-        if args.sensor_ids is not None:
-            sensor_ids = args.sensor_ids
+        args = input_schema.load({
+            **request.args,
+            "location_id": location_id,
+            "sensor_ids": request.args.getlist("sensor_ids"),
+        })
+        if args["sensor_ids"] is not None:
+            sensor_ids = args["sensor_ids"]
         else:
             sensor_ids = get_sensor_ids(location_id)
             if sensor_ids is None:
                 return (
-                    "Cannot get traffic for location that does not exist.",
+                    "Cannot get traffic count for location that does not exist.",
                     HTTPStatus.NOT_FOUND,
                 )
         if len(sensor_ids) == 0:
             return (
-                "Cannot get traffic for location that has 0 registered sensors",
+                "Cannot get traffic count for location that has 0 registered sensors",
                 HTTPStatus.BAD_REQUEST,
             )
-        response = get_from_data_store(input_schema.dump(args))
+        response = get_from_data_store(DatastoreEndpointEnum.TRAFFIC_COUNT.value, input_schema.dump(args))
         if response.status_code == HTTPStatus.OK:
             output_schema = TrafficCountSchema()
-            output = output_schema.load(response.json())
+            output = output_schema.load({
+                **response.json(), "locationId": location_id,
+            })
             return (
                 jsonify(output_schema.dump(output)),
                 HTTPStatus.OK,
@@ -103,38 +125,90 @@ def get_traffic_count(location_id):
             HTTPStatus.BAD_REQUEST,
         )
 
-@traffic_bp_route("peak_traffic", methods["GET"])
+@traffic_bp.route("peak_traffic", methods=["GET"])
 def get_peak_traffic(location_id):
-    if "sensor_ids" in request.args:
-        sensor_ids = request.args.getlist("sensor_ids", None)
-    else:
-        sensor_ids = get_sensor_ids(location_id)
-        if sensor_ids is None:
+    input_schema = PeakTrafficInputSchema()
+    try:
+        args = input_schema.load({
+            **request.args,
+            "location_id": location_id,
+            "sensor_ids": request.args.getlist("sensor_ids"),
+        })
+        if args["sensor_ids"] is not None:
+            sensor_ids = args["sensor_ids"]
+        else:
+            sensor_ids = get_sensor_ids(location_id)
+            if sensor_ids is None:
+                return (
+                    "Cannot get peak traffic for location that does not exist.",
+                    HTTPStatus.NOT_FOUND,
+                )
+        if len(sensor_ids) == 0:
             return (
-                "Cannot get traffic for location that does not exist.",
-                HTTPStatus.NOT_FOUND,
+                "Cannot get peak traffic for location that has 0 registered sensors",
+                HTTPStatus.BAD_REQUEST,
             )
-    if len(sensor_ids) == 0:
+        response = get_from_data_store(DatastoreEndpointEnum.PEAK_TRAFFIC.value, input_schema.dump(args))
+        if response.status_code == HTTPStatus.OK:
+            output_schema = PeakTrafficSchema()
+            output = output_schema.load({
+                **response.json(), "locationId": location_id,
+            })
+            return (
+                jsonify(output_schema.dump(output)),
+                HTTPStatus.OK,
+            )
         return (
-            "Cannot get traffic for location that has 0 registered sensors",
+            response.text,
+            response.status_code
+        )
+    except ValidationError as error:
+        print("ValidationError: Cannot get traffic: ", error.messages) # TODO: Implement logging
+        return (
+            "Cannot get traffic for requested location. Invalid request",
             HTTPStatus.BAD_REQUEST,
         )
 
-
-@traffic_bp_route("traffic_history", methods["GET"])
+@traffic_bp.route("traffic_history", methods=["GET"])
 def get_traffic_history(location_id):
-    if "sensor_ids" in request.args:
-        sensor_ids = request.args.getlist("sensor_ids", None)
-    else:
-        sensor_ids = get_sensor_ids(location_id)
-        if sensor_ids is None:
+    input_schema = TrafficHistoryInputSchema()
+    try:
+        args = input_schema.load({
+            **request.args,
+            "location_id": location_id,
+            "sensor_ids": request.args.getlist("sensor_ids"),
+        })
+        if args["sensor_ids"] is not None:
+            sensor_ids = args["sensor_ids"]
+        else:
+            sensor_ids = get_sensor_ids(location_id)
+            if sensor_ids is None:
+                return (
+                    "Cannot get traffic history for location that does not exist.",
+                    HTTPStatus.NOT_FOUND,
+                )
+        if len(sensor_ids) == 0:
             return (
-                "Cannot get traffic for location that does not exist.",
-                HTTPStatus.NOT_FOUND,
+                "Cannot get traffic history for location that has 0 registered sensors",
+                HTTPStatus.BAD_REQUEST,
             )
-    if len(sensor_ids) == 0:
+        response = get_from_data_store(DatastoreEndpointEnum.TRAFFIC_HISTORY.value, input_schema.dump(args))
+        if response.status_code == HTTPStatus.OK:
+            output_schema = TrafficHistorySchema()
+            output = output_schema.load({
+                **response.json(), "locationId": location_id,
+            })
+            return (
+                jsonify(output_schema.dump(output)),
+                HTTPStatus.OK,
+            )
         return (
-            "Cannot get traffic for location that has 0 registered sensors",
+            response.text,
+            response.status_code
+        )
+    except ValidationError as error:
+        print("ValidationError: Cannot get traffic history: ", error.messages) # TODO: Implement logging
+        return (
+            "Cannot get traffic for requested location. Invalid request",
             HTTPStatus.BAD_REQUEST,
         )
-    pass
--- tests/routes/test_traffic.py
+++ tests/routes/test_traffic.py
@@ -2,11 +2,134 @@
 import pytest
 import json
 import uuid
+import datetime as dt
 from http import HTTPStatus
 
 from marshmallow import ValidationError
 
 from tests.test_app import app
+from app.routes.traffic import (
+    DatastoreEndpointEnum,
+    TrafficCountSchema,
+    PeakTrafficSchema,
+    TrafficHistorySchema,
+)
 
-def test_can_fetch_traffic_for_location(app, monkeypatch):
-    pass
+class MockResponse:
+    def __init__(self, enum, error=False, mock_json={}):
+        self.enum = enum
+        self.status_code = 200
+        if error:
+            self.text = "generic error"
+            self.status_code = 500
+        self.mock_json = mock_json
+
+    def json(self):
+        return self.mock_json
+
+
+def test_can_fetch_traffic_count_for_location(app, monkeypatch):
+    sensor_ids = [str(uuid.uuid4()), str(uuid.uuid4())]
+    loc_id = str(uuid.uuid4())
+    data_in = {
+        "time": dt.datetime.utcnow().isoformat(),
+        "sensor_ids": sensor_ids,
+    }
+
+    response_json =  {
+        "time": dt.datetime.utcnow().isoformat(),
+        "fetchedAt": dt.datetime.utcnow().isoformat(),
+        "trafficCount": 20,
+    }
+
+    expected_response = {
+        **response_json,
+        "locationId": loc_id,
+    }
+    def mock_get(endpoint, *args, **kwargs):
+        return MockResponse(DatastoreEndpointEnum.TRAFFIC_COUNT, mock_json=response_json)
+
+    monkeypatch.setattr("requests.get", mock_get)
+    response = app.get(
+        "/api/locations/{}/traffic_count".format(loc_id),
+        query_string=data_in
+    )
+    assert response.status_code == HTTPStatus.OK
+    schema = TrafficCountSchema()
+    assert schema.loads(response.data) == schema.load(expected_response)
+
+
+def test_can_fetch_peak_traffic_for_location(app, monkeypatch):
+    sensor_ids = [str(uuid.uuid4()), str(uuid.uuid4())]
+    loc_id = str(uuid.uuid4())
+    data_in = {
+        "start_time": dt.datetime.utcnow().isoformat(),
+        "end_time": dt.datetime.utcnow().isoformat(),
+        "sensor_ids": sensor_ids,
+    }
+
+    response_json =  {
+        "startTime": data_in["start_time"],
+        "endTime": data_in["end_time"],
+        "fetchedAt": dt.datetime.utcnow().isoformat(),
+        "peakTraffic": {
+            "time": dt.datetime.utcnow().isoformat(),
+            "trafficCount": 20,
+        },
+    }
+
+    expected_response = {
+        **response_json,
+        "locationId": loc_id,
+    }
+
+    def mock_get(endpoint, *args, **kwargs):
+        return MockResponse(DatastoreEndpointEnum.PEAK_TRAFFIC, mock_json=response_json)
+
+    monkeypatch.setattr("requests.get", mock_get)
+    response = app.get(
+        "/api/locations/{}/peak_traffic".format(loc_id),
+        query_string=data_in
+    )
+    assert response.status_code == HTTPStatus.OK
+    schema = PeakTrafficSchema()
+    assert schema.loads(response.data) == schema.load(expected_response)
+
+def test_can_fetch_traffic_history_for_location(app, monkeypatch):
+    sensor_ids = [str(uuid.uuid4()), str(uuid.uuid4())]
+    loc_id = str(uuid.uuid4())
+    data_in = {
+        "start_time": dt.datetime.utcnow().isoformat(),
+        "end_time": dt.datetime.utcnow().isoformat(),
+        "sensor_ids": sensor_ids,
+    }
+
+    response_json =  {
+        "startTime": data_in["start_time"],
+        "endTime": data_in["end_time"],
+        "fetchedAt": dt.datetime.utcnow().isoformat(),
+        "trafficHistory": [{
+            "time": dt.datetime.utcnow().isoformat(),
+            "trafficCount": 20,
+        }, {
+            "time": dt.datetime.utcnow().isoformat(),
+            "trafficCount": 5,
+        }],
+    }
+
+    expected_response = {
+        **response_json,
+        "locationId": loc_id,
+    }
+
+    def mock_get(endpoint, *args, **kwargs):
+        return MockResponse(DatastoreEndpointEnum.PEAK_TRAFFIC, mock_json=response_json)
+
+    monkeypatch.setattr("requests.get", mock_get)
+    response = app.get(
+        "/api/locations/{}/traffic_history".format(loc_id),
+        query_string=data_in
+    )
+    assert response.status_code == HTTPStatus.OK
+    schema = TrafficHistorySchema()
+    assert schema.loads(response.data) == schema.load(expected_response)
